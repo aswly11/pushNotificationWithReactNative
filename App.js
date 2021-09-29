c788b89e7482e64f638853339233f23730d75e40
aswly11 <71109497+aswly11@users.noreply.github.com>
2021-09-29 06:33:38 +0200

    master

--- App.js
+++ App.js
@@ -1,11 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React ,{useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Notifications from 'expo-notifications';
+import * as Permissions from 'expo-permissions';
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+  shouldShowAlert: true,
+  shouldPlaySound: false,
+  shouldSetBadge: false,
+  }),
+ })
+ 
 export default function App() {
+const once= "";
+  useEffect(() => {
+    registerForPushNotification().then(token=>register(token)).catch(err=>console.log(Err))
+    
+  }, [once])
+  async function register(token)
+  {
+    console.log(token);
+    if(token!==null)
+    {
+      try{
+        const res= await fetch('http://fawrytest1-001-site1.htempurl.com/api/Notifications/InsertRegisterationToken?Token='+token,{
+          method:'post',
+          headers:{
+            'Accept':'application/json',
+            'Content-Type':'application/json'
+          }
+         
+      });
+      if(res.ok())
+      {
+        console.log('api posted successfully');
+      }
+      }catch(e){
+  console.log(e);
+      }
+    }
+
+  }
+  async function registerForPushNotification(){
+    const {status}=await Permissions.getAsync(Permissions.NOTIFICATIONS);
+    if(status!='granted')
+    {
+      const {status}=await Permissions.askAsync(Permissions.NOTIFICATIONS);
+    } 
+    
+    if(status!='granted')
+    {
+     alert('fail to get push token')
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+ 
+  
+    return token;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>hi</Text>
       <StatusBar style="auto" />
     </View>
   );
